178c85a739c0403b4a6204a6f48f9e2f43501c31
JustFuknDevIt <justfukndevit@gmail.com>
2020-12-12 16:15:27 +0100

    change a bit the server mail links

--- pages/authentication/[type].js
+++ pages/authentication/[type].js
@@ -6,7 +6,10 @@ const Authentication = ({ queryType, token }) => {
 		<div className="flex flex-col w-screen h-screen justify-center items-center">
 			<main className="h-2/5 flex flex-col justify-around">
 				{queryType === "finish" ? (
-					<Big>Please wait until the redirect...</Big>
+					<Big>
+						Please wait until the redirect...
+						<br /> {token}
+					</Big>
 				) : (
 					<AuthForm type={queryType} />
 				)}
@@ -19,8 +22,10 @@ export async function getServerSideProps(context) {
 	const queryType = context.query.type;
 	const token = context.query.token ? context.query.token : null;
 
+	// make the verification of the token
+	// send cookies and redirect user
 	return {
-		props: { queryType, token }, // will be passed to the page component as props
+		props: { queryType, token },
 	};
 }
 
