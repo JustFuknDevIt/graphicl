4751a5c516fbbeb186fc735c8fe4fe159497b3b3
JustFuknDevIt <justfukndevit@gmail.com>
2020-12-21 12:47:29 +0100

    protect the router for unauthorized users

--- pages/feedback.js
+++ pages/feedback.js
@@ -1,33 +1,40 @@
 import { useQuery } from "@apollo/client";
 import { GET_USERS } from "graphql/client/queries";
+import { useAuth } from "components/AuthProvider";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import Link from "next/link";
 
 export const Feedback = ({}) => {
+	const router = useRouter();
+	const { authState } = useAuth();
 	const { loading, error, data } = useQuery(GET_USERS, {
 		errorPolicy: "all",
 	});
+	useEffect(() => {
+		if (!authState.isAuth) {
+			router.push("/");
+		}
+	});
+
+	error && (
+		<div>
+			{error.graphQLErrors.map(({ message }, i) => (
+				<span key={i}>{message}</span>
+			))}
+		</div>
+	);
+	loading && <div>Loading</div>;
 
 	return (
-		<>
-			{error && (
-				<div>
-					{error.graphQLErrors.map(({ message }, i) => (
-						<span key={i}>{message}</span>
-					))}
-				</div>
-			)}
-			{loading && <div>Loading</div>}
-			{data && (
-				<div>
-					List of Users :
-					{data &&
-						data.getUsers.map((user, i) => {
-							return <div key={i}>{user.username}</div>;
-						})}
-					<Link href="/">Go Home</Link>
-				</div>
-			)}
-		</>
+		data & authState.isAuth && (
+			<>
+				{data.getUsers.map((user, i) => {
+					return <div key={i}>{user.username}</div>;
+				})}
+				<Link href="/">Go Home</Link>
+			</>
+		)
 	);
 };
 
--- pages/home.js
+++ pages/home.js
@@ -1,4 +1,17 @@
+import { useAuth } from "components/AuthProvider";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 const Home = ({}) => {
+	const router = useRouter();
+	const { authState } = useAuth();
+	console.log(authState);
+
+	useEffect(() => {
+		if (!authState.isAuth) {
+			router.push("/");
+		}
+	});
 	return <div>Bienvenue sur Home page de Graphicl</div>;
 };
 
